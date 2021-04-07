39c1766cbaf3aaf3363ee8f0004c4ddea5d36ee4
KulaBab <82053505+KulaBab@users.noreply.github.com>
2021-04-07 14:57:01 +0300

    Update ImagineLoader.lua

--- scripts/ImagineLoader.lua
+++ scripts/ImagineLoader.lua
@@ -2,6 +2,10 @@ if game.PlaceId == 5627314498 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker-TA/ScriptCenter/main/scripts/ImagineLobby.lua",true))();
 end
 
-if game.PlaceId == 5634547605 or 5739953467 then
+if game.PlaceId == 5634547605 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker-TA/ScriptCenter/main/scripts/Imagine.lua",true))();
+end
+
+if game.PlaceId == 5739953467 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker-TA/ScriptCenter/main/scripts/Imagine.lua",true))();
 end
