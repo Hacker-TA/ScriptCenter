c5acc850adcfd5a4004db3d391a6e02992f01d64
KulaBab <82053505+KulaBab@users.noreply.github.com>
2021-04-07 12:47:22 +0300

    Create IsleLoader.lua

---
+++ scripts/IsleLoader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 3431407618 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker-TA/ScriptCenter/main/scripts/Isle.lua",true))();
+end
+ 
+if game.PlaceId == 3095204897 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker-TA/ScriptCenter/main/scripts/IsleLobby.lua",true))();
+end
